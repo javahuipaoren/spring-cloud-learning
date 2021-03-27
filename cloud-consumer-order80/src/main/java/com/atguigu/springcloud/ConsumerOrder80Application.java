9470334cfe295416a027b8813f3abe3852084e7e
chenmeng <554045974@qq.com>
2021-03-28 01:22:24 +0800

    自定义ribbon负载均衡规则

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/ConsumerOrder80Application.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/ConsumerOrder80Application.java
@@ -1,11 +1,14 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.rule.RuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = RuleConfig.class)
 public class ConsumerOrder80Application {
 
     public static void main(String[] args) {
