d385683d28c5db9477db8e41499adf3624a79ecf
chenmeng <554045974@qq.com>
2021-03-31 02:10:30 +0800

    git配置中心config集成spring-cloud

---
+++ cloud-config-client-3355/src/main/java/com/atgui/springcloud/ConfigClient3355Main.java
@@ -0,0 +1,9 @@
+package com.atgui.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClient3355Main {
+}
